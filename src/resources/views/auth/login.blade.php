fe83e7156f3886ef8efc47b7f88bb6c87e69a68e
Marcio Mota <mjpinto@latam.stefanini.com>
2022-12-07 11:07:19 -0300

    add auth templates

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,70 +1,77 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div>
+    <div class="page main-signin-wrapper">
+        <div class="signpages text-center row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="row-sm row">
+                        <div class="d-none d-lg-block text-center bg-primary details col-xl-5 col-lg-6">
+                            <div class="mt-5 pt-4 p-2 pos-absolute" style="width: 90%">
+                                <img src="{{url('/images/logo-white.png')}}" alt="Logo Hemominas" class="header-brand-img mb-4" />
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <div class="clearfix"></div>
+                                <img src="{{url('/images/user-shield.svg')}}" alt="user" class="ht-100 mb-0" />
+                                <h5 class="mt-4 text-white">Autenticador</h5>
+                                <span class="tx-white-6 tx-13 mb-5 mt-xl-0">
+                                    Acesse os sistemas da Fundação Hemominas.
+                                </span>
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="login_form col-xl-7 col-lg-6 col-sm-12 col-12">
+                            <div class="container-fluid">
+                                <div class="row-sm row">
+                                    <div class="mt-2 mb-2 card-body">
+                                        <img src="{{url('/images/logo.png')}}" alt="Logo Hemominas" class="d-lg-none header-brand-img.text-start" />
+                                        <div class="clearfix"></div>
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
+                                            <h5 class="text-start mb-2">Login de acesso</h5>
+                                            <p class="mb-4 text-muted tx-13 ms-0 text-start">
+                                                Acesse os sistemas de gestão da Fundação Hemominas.
+                                            </p>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                            <div class="text-start form-group">
+                                                <label for="masp" class="form-label">Código masp</label>
+                                                <input type="text" name="masp" id="masp" class="form-control @error('masp') is_invalid @enderror" value="{{old('masp')}}" required autofocus>
+                                                @error('masp')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                                            <div class="text-start form-group">
+                                                <label for="password" class="form-label">Senha</label>
+                                                <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" required>
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <button type="button" class="btn ripple btn-main-primary btn-main-primary btn-block mt-2 bt btn-primary">
+                                                Login
+                                            </button>
+                                        </form>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                        <div class="text-start mt-5 ms-0">
+                                            <div class="mb-1">
+                                                <a href="#">Esqueceu a senha?</a>
+                                            </div>
+
+                                            <div>
+                                                Não tem conta? <a href="#"> solicite seu acesso aqui!   </a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        https: false,
+        host: '0.0.0.0',
+        hmr: {
+            host: 'gandauth.stefanini.test'
+        },
+        watch: {
+            usePolling: true,
+        }
+    },
     plugins: [
         laravel({
             input: [
