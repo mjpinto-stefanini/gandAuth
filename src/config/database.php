16b652b8b28106ee570a62771b86765831c1d730
Deivid Schlosser Onorio <dsonorio@stefanini.com>
2023-04-27 14:45:16 -0300

    Add ldap - ldaprecord-laravel

--- src/config/database.php
+++ src/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'otacle'),
+    'default' => env('DB_CONNECTION', 'oracle'),
 
     /*
     |--------------------------------------------------------------------------
